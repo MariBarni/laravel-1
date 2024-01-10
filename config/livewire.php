9e562b566ea71bff5dfcafd63ffc7b5d15964538
Mariuxi <mariuxi.barnikel-benalcazar@stimme.de>
2024-01-10 09:05:01 +0100

    livewire

--- config/livewire.php
+++ config/livewire.php
@@ -98,14 +98,14 @@ return [
     'temporary_file_upload' => [
         'disk' => null,        // Example: 'local', 's3'              Default: 'default'
         'rules' => 'required', 'file|mimes:png,jpg,jpeg', 'max:20480',       // Example: ['file', 'mimes:png,jpg']  Default: ['required', 'file', 'max:20480'] (20MB)
-        'directory' => 'tmp',   // Example: 'tmp'                      Default  'livewire-tmp'
-        'middleware' => 'throttle:5,1',  // Example: 'throttle:5,1'             Default: 'throttle:60,1'
+        'directory' => 'default',   // Example: 'tmp'                      Default  'livewire-tmp'
+        'middleware' => 'throttle:15,1',  // Example: 'throttle:5,1'             Default: 'throttle:60,1'
         'preview_mimes' => [   // Supported file types for temporary pre-signed file URLs.
             'png', 'gif', 'bmp', 'svg', 'wav', 'mp4',
             'mov', 'avi', 'wmv', 'mp3', 'm4a',
             'jpg', 'jpeg', 'mpga', 'webp', 'wma',
         ],
-        'max_upload_time' => 5, // Max duration (in minutes) before an upload gets invalidated.
+        'max_upload_time' => 10, // Max duration (in minutes) before an upload gets invalidated.
     ],
 
     /*
